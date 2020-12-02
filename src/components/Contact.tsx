6c82cfeca2be7444b83a65a9105d094805857e5e
Dalekvim <dalekvim@gmail.com>
2020-12-02 16:12:03 +0000

    added an interface

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
 
-export class Contact extends React.Component {
-    clubMembers = [
-        {
-            name: "Vimal Vinod",
-            email: "vvinod@wigstonstudents.org"
-        }
-    ]
+interface IContactProps {
+}
+interface IContactState {
+    members: {
+        name: string,
+        email: string
+    }[]
+}
+export class Contact extends React.Component<IContactProps, IContactState> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            members: []
+        };
+    }
+    fetchData() {
+        fetch("http://localhost:3000/api/members").then(
+            res => res.json()
+        ).then(
+            res => this.setState(res)
+        ).then(
+            () => console.log(this.state)
+        )
+    }
+    componentDidMount() {
+        this.fetchData();
+    }
     render() {
         return (
             <div className="Contact">
                 <h1>Contact Us</h1>
-                <div className="ContactDetails">
+                <div className="ContactDetails">              
                     {
-                        this.clubMembers.map(member => (
-                                <div className={`media ${member.name}`}>
+                        this.state.members.map(member => (
+                                <div className='media' key={`${ member.name }`}>
                                     <img src="..." className="mr-3" alt="This bit's not working yet." />
                                     <div className="media-body">
-                                        <h4 className="mt-0">{ member.name }</h4>
-                                        <h5>{ member.email }</h5>
+                                        <h5 className="mt-0">{ member.name }</h5>
+                                        { member.email }
                                     </div>
                                 </div>
                             )
